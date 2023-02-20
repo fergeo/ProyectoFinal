cc096b0212f3a5e180655f7e3b7d4749b6a82274
fergeo <fergeo@yahoo.com>
2023-02-20 10:47:02 -0300

    Redireccionamiento ok sin parametros

--- js/logOn.js
+++ js/logOn.js
@@ -4,60 +4,77 @@ var emails = [];
 var exprecionReg;
 var valido;
 var contrasena;
-var nombreApellido;
+var nombreApellido = [];
 var correoElectronico;
 var mensaje = 'Debe ingresar';
 
 function validar(formulario) {
-    
     //Validacion de la correo
-      if( formulario.inputEmail.value ){
+    if( formulario.inputEmail.value ){
     
-      //Expresion regular del correo
-        exprecionReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-        valido = exprecionReg.test(formulario.inputEmail.value);
-        if(valido==false){
-          alert('Correo invalido. Ingrese correctamente el correo electronico');
-        }
+        //Expresion regular del correo
+          exprecionReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+          valido = exprecionReg.test(formulario.inputEmail.value);
+          if(valido==false){
+            alert('Correo invalido. Ingrese correctamente el correo electronico');
+          }
+        
+        }else{
+          mensaje = mensaje + ' un correo electronico,'
+      }  
+  
+          //Carga los datos que estan en el JSON
+          peticion = new XMLHttpRequest();
+          peticion.open('GET','http://127.0.0.1:5500/emailsValidos.json');
+         
+          peticion.onload = function(){
+            
+              if( peticion.status == 200 ){
+            
+                  //Guarda el resultado en variables
+                  datos = JSON.parse(peticion.response);
+            
+                  datos.emails.forEach(function(element){
+                      emails.push(element);  
+                  });
       
-      }else{
-        mensaje = mensaje + ' un correo electronico,'
-    }  
+      console.log("valor")
+                  emails.forEach(function(element){
+                    console.log(element)
+                      if(element.email == formulario.inputEmail.value && element.password == formulario.inputPassword.value){
+                  
+                          nombreApellido = element.nomyape;
+                          correoElectronico = formulario.inputEmail.value;
+                      }
+  
+                  });
 
-        //Carga los datos que estan en el JSON
-        peticion = new XMLHttpRequest();
-        peticion.open('GET','http://127.0.0.1:5500/info.json');
-       
-        peticion.onload = function(){
-          
-            if( peticion.status == 200 ){
-          
-                //Guarda el resultado en variables
-                datos = JSON.parse(peticion.response);
-          
-                datos.emails.forEach(function(element){
-                    emails.push(element);  
-                });
-    
-    
-                emails.forEach(function(element){
-                    if(element.emails == formulario.inputEmail.value){
-                        nombreApellido = element.nomyape;
-                        correoElectronico = formulario.inputEmail.value;
-                    }
+                  
 
-                });
-    
-            }  
-        };
-        peticion.send();  
+                  console.log("valor")
+                  console.log(nombreApellido)
 
-        if(nombreApellido.length() > 0){
-            "index.html?nomyape=${nombreApellido},emails=${correoElectronico}"
-        } else{
-            "No existe"
-        }
+                  if(nombreApellido.length > 0){
+                    //alert("link")
+                    //location.href
+                    window.location.href = "http://127.0.0.1:5500/index.html"
+                } else{
+                  alert("1")
+                }
+            
+
+      
+              }  
+          };
+          peticion.send();  
+
+          
+
+         
+            alert("finalizado")
+         
     
+   
 }
 
 
