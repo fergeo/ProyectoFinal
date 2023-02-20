7cec65aa19ccb2bb950dd27d1cf2ee728b7cfa83
fergeo <fergeo@yahoo.com>
2023-02-19 21:03:54 -0300

    Valida el logOn contra el Jason

--- js/logOn.js
+++ js/logOn.js
@@ -4,60 +4,65 @@ var emails = [];
 var exprecionReg;
 var valido;
 var contrasena;
-var nombreApellido;
+var nombreApellido = [];
 var correoElectronico;
 var mensaje = 'Debe ingresar';
 
 function validar(formulario) {
-    
     //Validacion de la correo
-      if( formulario.inputEmail.value ){
+    if( formulario.inputEmail.value ){
     
-      //Expresion regular del correo
-        exprecionReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-        valido = exprecionReg.test(formulario.inputEmail.value);
-        if(valido==false){
-          alert('Correo invalido. Ingrese correctamente el correo electronico');
-        }
+        //Expresion regular del correo
+          exprecionReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+          valido = exprecionReg.test(formulario.inputEmail.value);
+          if(valido==false){
+            alert('Correo invalido. Ingrese correctamente el correo electronico');
+          }
+        
+        }else{
+          mensaje = mensaje + ' un correo electronico,'
+      }  
+  
+          //Carga los datos que estan en el JSON
+          peticion = new XMLHttpRequest();
+          peticion.open('GET','http://127.0.0.1:5500/emailsValidos.json');
+         
+          peticion.onload = function(){
+            
+              if( peticion.status == 200 ){
+            
+                  //Guarda el resultado en variables
+                  datos = JSON.parse(peticion.response);
+            
+                  datos.emails.forEach(function(element){
+                      emails.push(element);  
+                  });
       
-      }else{
-        mensaje = mensaje + ' un correo electronico,'
-    }  
+      console.log("valor")
+                  emails.forEach(function(element){
+                    console.log(element)
+                      if(element.email == formulario.inputEmail.value && element.password == formulario.inputPassword.value){
+                  
+                          nombreApellido = element.nomyape;
+                          correoElectronico = formulario.inputEmail.value;
+                      }
+  
+                  });
+      
+              }  
+          };
+          peticion.send();  
 
-        //Carga los datos que estan en el JSON
-        peticion = new XMLHttpRequest();
-        peticion.open('GET','http://127.0.0.1:5500/info.json');
-       
-        peticion.onload = function(){
-          
-            if( peticion.status == 200 ){
-          
-                //Guarda el resultado en variables
-                datos = JSON.parse(peticion.response);
-          
-                datos.emails.forEach(function(element){
-                    emails.push(element);  
-                });
-    
-    
-                emails.forEach(function(element){
-                    if(element.emails == formulario.inputEmail.value){
-                        nombreApellido = element.nomyape;
-                        correoElectronico = formulario.inputEmail.value;
-                    }
+          console.log(nombreApellido)
 
-                });
-    
-            }  
-        };
-        peticion.send();  
-
-        if(nombreApellido.length() > 0){
-            "index.html?nomyape=${nombreApellido},emails=${correoElectronico}"
+          if(nombreApellido){
+            alert("1")
+            //"index.html"
         } else{
-            "No existe"
+            alert("2")
         }
     
+   
 }
 
 
